4780a10e373a3625d39b86a71f7f0876d770dd78
ACAPP-dev <acapp909@gmail.com>
2020-04-21 15:04:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+const nameArray = ['Andrew', 'Sharon', 'Amber', 'Ashleigh', 'Jessica', 'Jolynn']
+
+let cardsArray = []
+
+function writeCards(names, event) {
+    for (const el of names) {
+        cardsArray.push(`Thank you, ${el}, for the wonderful ${event} gift!`);
+    }
+    
+    return cardsArray
+}
+
+// console.log(writeCards(nameArray, 'birthday'))
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number)
+        number -= 1
+    }
+}
